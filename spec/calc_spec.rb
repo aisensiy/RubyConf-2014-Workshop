9df382e7f7673834e57f3f3955aba71904369896
Di Wang <wayne.wangdi@yandex.com>
2014-11-02 15:01:33 +0800

    more tests for arithmetic operation of a pair of numbers

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -38,4 +38,24 @@ describe 'calc' do
   it 'should eval 4/3 to 1' do
     Calc.eval('4/3').should be 1
   end
+
+  it 'should eval 11+9 to 20' do
+    Calc.eval('11+9').should be 20
+  end
+
+  it 'should eval 30-11 to 19' do
+    Calc.eval('30-11').should be 19
+  end
+
+  it 'should eval 11*12 to 132' do
+    Calc.eval('11*12').should be 132
+  end
+
+  it 'should eval 15/3 to 5' do
+    Calc.eval('15/3').should be 5
+  end
+
+  it 'should eval 15/6 to 2' do
+    Calc.eval('15/6').should be 2
+  end
 end
