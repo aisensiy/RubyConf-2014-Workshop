8820327813ff3be7f2b9156b80c62990522456ff
aisensiy <aisensiy@gmail.com>
2014-11-02 15:09:51 +0800

    无额外符号的四则运算

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,5 +1,24 @@
 module Calc
   def self.eval(string)
-    string.to_i
+    opt_pos = string =~ /[\+\-\*\/]/
+    if !opt_pos.nil?
+
+      left = string[0...opt_pos].to_i
+      opt = string[opt_pos]
+      right = string[(opt_pos + 1)...string.size].to_i
+
+      if opt == '+'
+        left + right
+      elsif opt == '*'
+        left * right
+      elsif opt == '/'
+        left / right
+      else
+        left - right
+      end
+
+    else
+      string.to_i
+    end
   end
 end
