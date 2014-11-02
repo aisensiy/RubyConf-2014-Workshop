3912bf6db56fa476f2ca38c69e8a435f5b170fd2
Di Wang <wayne.wangdi@yandex.com>
2014-11-02 16:31:31 +0800

    handle mulitple operations in a expression

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,21 +1,70 @@
 module Calc
-  def self.eval(string)
-    opt_pos = string =~ /[\+\-\*\/]/
-    if !opt_pos.nil?
-
-      opt = string[opt_pos]
-
-      values = string.split(opt).map(&:to_i)
-
-      case opt
-        when '+' then values.reduce(:+)
-        when '-' then [0, values.reduce(:-)].max
-        when '*' then values.reduce(:*)
-        when '/' then values.reduce(:/)
-      end
-
-    else
-      string.to_i
+  class AddOperation
+    def self.regexp
+      /\+/
     end
+
+    def self.combine(a, b)
+      a + b
+    end
+  end
+
+  class SubOperation
+    def self.regexp
+      /\-/
+    end
+
+    def self.combine(a, b)
+      [a - b, 0].max
+    end
+  end
+
+  class MulOperation
+    def self.regexp
+      /\*/
+    end
+
+    def self.combine(a, b)
+      a * b
+    end
+  end
+
+  class DivOperation
+    def self.regexp
+      /\//
+    end
+
+    def self.combine(a, b)
+      a / b
+    end
+  end
+
+  class ::String
+    def last_operation(operation_class)
+      head = self =~ operation_class.regexp
+      tail = self[(head + 1)..-1].last_operation(operation_class) unless head.nil?
+      tail += head + 1 unless tail.nil? or tail < 0
+
+      result = head
+      result = tail unless tail.nil? or tail < 0
+      result = -1 if head.nil?
+      result
+    end
+
+    def parse
+      operation_pairs = [AddOperation, SubOperation, MulOperation, DivOperation].map { |klass| [klass, self.last_operation(klass)] }
+      operation_pair = operation_pairs.max_by { |p| p[-1] }
+      if operation_pair[-1] < 0
+        self.to_i
+      else
+        operation_class = operation_pair[0]
+        operation_pos = operation_pair[1]
+        operation_class.combine(self[0..(operation_pos - 1)].parse, self[(operation_pos + 1)..-1].to_i)
+      end
+    end
+  end
+
+  def self.eval(string)
+    string.parse
   end
 end
