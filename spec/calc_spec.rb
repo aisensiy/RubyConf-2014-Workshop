52a12a5b67fe76530053573f3fe96b68ae5e40e8
Di Wang <wayne.wangdi@yandex.com>
2014-11-02 14:48:32 +0800

    add tests for arithmetic operation of a pair of numbers

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -18,4 +18,24 @@ describe 'calc' do
   it 'should eval 22 to 22' do
     Calc.eval('22').should be 22
   end
+
+  it 'should eval 1+2 to 3' do
+    Calc.eval('1+2').should be 3
+  end
+
+  it 'should eval 1-1 to 0' do
+    Calc.eval('1-1').should be 0
+  end
+
+  it 'should eval 2*1 to 2' do
+    Calc.eval('2*1').should be 2
+  end
+
+  it 'should eval 4/2 to 2' do
+    Calc.eval('4/2').should be 2
+  end
+
+  it 'should eval 4/3 to 1' do
+    Calc.eval('4/3').should be 1
+  end
 end
