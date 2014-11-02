d0a0fd881b4b2ff90fab3ea70d5a974ad175db4b
aisensiy <aisensiy@gmail.com>
2014-11-02 14:41:50 +0800

    parse int

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -3,7 +3,19 @@ require 'calc'
 
 describe 'calc' do
 
+  it 'should eval 1 to 1' do
+    Calc.eval('1').should be 1
+  end
+
   it 'should eval 2 to 2' do
-    Calc.eval('2') == 2
+    Calc.eval('2').should be 2
+  end
+
+  it 'should eval 11 to 11' do
+    Calc.eval('11').should be 11
+  end
+
+  it 'should eval 22 to 22' do
+    Calc.eval('22').should be 22
   end
 end
